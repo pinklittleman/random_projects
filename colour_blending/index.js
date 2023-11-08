628e729ddd78bc77d4b8e6f5ce7b9cb97097a7e0
theo <80356772+pinklittleman@users.noreply.github.com>
2023-11-08 05:29:03 +0000

    changing a few things and adding new stuff

---
+++ colour_blending/index.js
@@ -0,0 +1,56 @@
+let canvas = document.querySelector("canvas")
+let ctx = canvas.getContext("2d")
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+let points = [], gen = 1, spot = 10
+
+class Point{
+    constructor(x,y,generation){
+        this.x = x
+        this.y = y
+        this.generation = generation
+        points.push(this)
+    }
+
+    draw(){
+        ctx.fillStyle = "rgb(200,20,20)"
+        ctx.fillRect(this.x, this.y, 5, 5)
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    new Point(randnum(canvas.width), 10, 1)
+}
+
+
+function mainLoop(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+
+    points.forEach(point => {
+        point.draw()
+    });
+
+    requestAnimationFrame(mainLoop)
+}
+
+function randnum(num){
+    let ran = Math.floor(Math.random()*num)
+    if (ran == 0){
+        ran++
+    }
+    return ran
+}
+
+function generate(){
+    gen++
+    console.log(gen)
+    points.forEach(point => {
+        if(gen == point.generation){
+            new Point(point.x+randnum(10), gen*10, gen)
+        }
+    });
+}
+
+requestAnimationFrame(mainLoop)
