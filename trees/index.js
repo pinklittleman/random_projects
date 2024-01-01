fb8ff78b555533102b8ad901b29d4b94187d1830
Theo Salisbury <theominer3000@gmail.com>
2024-01-01 17:59:33 +0000

    some small optimizations

--- trees/index.js
+++ trees/index.js
@@ -15,20 +15,17 @@ let trees = {
     4:{Hchestnut: []}
 }
 
-let num = 0
-let tile_X_calculation = Math.floor(canvas.width/tile_size)
-let tile_X_remainder = (canvas.width%tile_size)
-let tile_X_new_remainder = canvas.width/tile_X_remainder
-let tile_X_displace_ammount = (tile_X_new_remainder/2)
-let final_X_tile_displace = tile_X_displace_ammount/tile_X_calculation
+let tile_X_calculation = Math.floor(canvas.width/tile_size)-2
+// let tile_X_remainder = (canvas.width%tile_size)
+// let tile_X_new_remainder = canvas.width/tile_X_remainder
+// let tile_X_displace_ammount = (tile_X_new_remainder/2)
+// let final_X_tile_displace = tile_X_displace_ammount/tile_X_calculation
 
-let tile_Y_calculation = Math.floor(canvas.height/tile_size)
-let tile_Y_remainder = (canvas.height%tile_size)*10
-let tile_Y_displace_ammount = tile_Y_remainder/2
-let final_Y_tile_displace = tile_Y_displace_ammount/tile_X_calculation
+let tile_Y_calculation = Math.floor(canvas.height/tile_size)-1
+// let tile_Y_remainder = (canvas.height%tile_size)*10
+// let tile_Y_displace_ammount = tile_Y_remainder/2
+// let final_Y_tile_displace = tile_Y_displace_ammount/tile_X_calculation
 
-let startX = canvas.width/2
-let startY = canvas.height/2
 let tiles = []
 
 class Tile{
@@ -41,8 +38,30 @@ class Tile{
             3:{x:this.centerX,y:this.centerY},
             4:{x:this.centerX+tile_size,y:this.centerY},
         }
-        tiles.push(this.dic)
+        this.RX = randnum(3)
+        this.RY = randnum(3)
+        tiles.push(this)
     }
+    draw() {
+        let based = 2
+        for (let i = 1; i < 5; i++) {
+            for (let j = 0; j < tiles.length; j++) {
+                ctx.beginPath()
+                ctx.strokeStyle = "rgb(200,10,10)"
+                ctx.fillStyle = "rgb(250,250,150)"
+                ctx.lineWidth = 2
+                ctx.moveTo(tiles[j].dic[i].x,tiles[j].dic[i].y)
+                ctx.lineTo(tiles[j].dic[based].x,tiles[j].dic[based].y)
+                ctx.stroke()
+            //  ctx.fillText(`${based}`,tiles[j].dic[i].x+tiles[j].RX,tiles[j].dic[i].y-tiles[j].RY)
+        }
+        if (based >= 4){
+            based = 0
+        }
+        based++
+    }
+    }
+
 }
 
 let centers = []
@@ -68,36 +87,19 @@ class seed{
 
 for (let y = 0; y < tile_Y_calculation; y++) {
    for (let x = 0; x < tile_X_calculation; x++) {
-        new Tile((x*tile_size)+tile_X_displace_ammount,y*tile_size+tile_size+20)
+        new Tile(x*tile_size*1,(y*tile_size+tile_size)*1)
    }
-    
 }
 
 function mainLoop(){
-    // ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    // this is just drawing 
     world.forEach(item => {
         item.draw()
     });
-    
-    //  just witchcraft
-    let based = 2
-    for (let i = 1; i < 4+1; i++) {
-        for (let j = 0; j < tiles.length; j++) {
-            ctx.beginPath()
-            ctx.strokeStyle = "rgb(200,10,10)"
-            ctx.fillStyle = "rgb(250,250,150)"
-            ctx.lineWidth = 2
-            ctx.moveTo(tiles[j][i].x,tiles[j][i].y)
-            ctx.lineTo(tiles[j][based].x,tiles[j][based].y)
-            ctx.stroke()
-            ctx.fillText(`${based}`,tiles[j][i].x+4,tiles[j][i].y-5)
-        }
-        if (based >= 4){
-            based = 0
-        }
-        based++
-    }
-
+    tiles.forEach(tile =>{
+        tile.draw()
+    })
 
     requestAnimationFrame(mainLoop)
 }
