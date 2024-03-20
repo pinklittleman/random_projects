9b797e48ab765c9f0fff5086958dd3968b4bc595
pinklittleman <based@based.com>
2024-03-20 15:12:44 +0000

    background type beat

--- rotating_square/index.js
+++ rotating_square/index.js
@@ -4,7 +4,7 @@ let ctx    = canvas.getContext("2d")
 canvas.width  = innerWidth
 canvas.height = innerHeight
 
-let Items = [], dlt = 0.01, maxItems = 500, colours = ["rgb(2, 48, 71)","rgb(142, 202, 230)","rgb(33, 158, 188)","rgb(255, 183, 3)","rgb(251, 133, 0)"]
+let Items = [], dlt = 0.009, maxItems = 400, colours = ["rgb(42, 72, 88)","rgb(255, 134, 20)","rgb(255, 7, 84)","rgb(236, 78, 126)","rgb(195, 81, 154)","rgb(143, 88, 161)","rgb(92, 89, 149)","rgb(54, 83, 121)"]
 
 class Square{
     constructor(x,y,startingAngle){
@@ -46,7 +46,46 @@ class Square{
     }
 }
 
-new Square(100,100,10)
+class Triangle {
+    constructor(x, y, startingAngle) {
+        this.x = x;
+        this.y = y;
+        this.length = 40;
+        this.angle = startingAngle;
+        this.centerX = this.x + this.length / 2;
+        this.centerY = this.y + (Math.sqrt(3) / 2) * this.length; // Adjusted for equilateral triangle
+        this.colour = colours[randnum(colours.length)]; // Assuming colours and randnum are defined elsewhere
+        Items.push(this); // Assuming Items array is defined elsewhere
+    }
+
+    draw() {
+        ctx.fillStyle = this.colour;
+        ctx.strokeStyle = this.colour;
+        ctx.beginPath();
+        let p1 = this.rotatePoint(this.x, this.y + this.length, this.centerX, this.centerY, this.angle); // Bottom-left corner
+        let p2 = this.rotatePoint(this.x + this.length / 2, this.y, this.centerX, this.centerY, this.angle); // Top corner
+        let p3 = this.rotatePoint(this.x + this.length, this.y + this.length, this.centerX, this.centerY, this.angle); // Bottom-right corner
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.lineTo(p3.x, p3.y);
+        ctx.closePath();
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        this.angle += dlt;
+        if (this.angle >= 360) {
+            this.angle -= 360;
+        }
+    }
+
+    rotatePoint(x, y, centerX, centerY, angle) {
+        let radians = angle * Math.PI / 180;
+        let cos = Math.cos(radians);
+        let sin = Math.sin(radians);
+        let nx = (cos * (x - centerX)) + (sin * (y - centerY)) + centerX;
+        let ny = (cos * (y - centerY)) - (sin * (x - centerX)) + centerY;
+        return { x: nx, y: ny };
+    }
+}
 
 function randnum(num){
     let ran = Math.floor(Math.random()*num)
@@ -59,8 +98,13 @@ function randnum(num){
 setInterval(() => {
     if(Items.length < maxItems){
         new Square(randnum(canvas.width),randnum(canvas.height),randnum(360))
+        new Triangle(randnum(canvas.width),randnum(canvas.height),randnum(360))
     }
 }, 100);
+setInterval(() => {
+    Items.splice(1,1)
+}, 3000);
+
 
 function mainloop(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
